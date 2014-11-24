853cb86ca68c7657361bfa24fcb577c2e8465ca8
anonymous <nguyentuananh1993@gmail.com>
2014-11-24 23:14:39 +0700

    update User

--- homework-week6-login-page/test/models/user_test.rb
+++ homework-week6-login-page/test/models/user_test.rb
@@ -45,6 +45,12 @@ test "name should not be too long" do
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 
     test "email addresses should be unique" do
     duplicate_user = @user.dup
